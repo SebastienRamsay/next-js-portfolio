9673c3b29bef8a1657474cfdb3f7437e939e4180
Sebastien Ramsay <ramsa0130@algonquinlive.com>
2023-10-28 22:28:45 -0400

    removed /headless from toast import

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 import { useSectionInView } from '../../lib/hooks';
 import { sendEmail } from '../../actions/sendEmail';
 import SubmitBtn from './submit-btn';
-import toast from 'react-hot-toast/headless';
+import toast from 'react-hot-toast';
 
 export default function Contact() {
   const { ref } = useSectionInView('Contact', 0.5);
