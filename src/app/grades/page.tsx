38d34d721e551050ef083ca4bc7537324921980c
Sebastien Ramsay <ramsa0130@algonquinlive.com>
2024-01-24 22:13:11 -0500

    fixing up grades page

--- src/app/grades/page.tsx
+++ src/app/grades/page.tsx
@@ -1,32 +1,37 @@
-'use client';
-
 import Image from 'next/image';
-import GradesPic from '../../../public/Computer-Science-Grades.png'
-import TranscriptPic1 from '../../../public/Transcript-1.png'
-import TranscriptPic2 from '../../../public/Transcript-2.png'
+import GradesPic from '../../../public/Computer-Science-Grades.png';
+import TranscriptPic1 from '../../../public/Transcript-1.png';
+import TranscriptPic2 from '../../../public/Transcript-2.png';
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center px-4">
-      <h1>Grades / Transcript</h1>
+    <main className="flex flex-col items-center gap-3 px-4 pb-4">
+      <h1 className="text-5xl">
+        <b>Grades / Transcript</b>
+      </h1>
       <Image
-              alt="Grades"
-              src={GradesPic}
-              quality="95"
-              priority={true}
-            />
-            <Image
-              alt="Transcript 1/2"
-              src={TranscriptPic1}
-              quality="95"
-              priority={true}
-            />
-            <Image
-              alt="Transcript 2/2"
-              src={TranscriptPic2}
-              quality="95"
-              priority={true}
-            />
+        alt="Grades"
+        src={GradesPic}
+        quality="100"
+        priority={true}
+        className="max-w-4xl"
+      />
+      <div className="flex flex-col gap-1">
+        <Image
+          alt="Transcript 1/2"
+          src={TranscriptPic1}
+          quality="100"
+          priority={true}
+          className="max-w-4xl"
+        />
+        <Image
+          alt="Transcript 2/2"
+          src={TranscriptPic2}
+          quality="100"
+          priority={true}
+          className="max-w-4xl"
+        />
+      </div>
     </main>
   );
 }
